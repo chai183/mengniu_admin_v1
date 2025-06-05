aa49d6889db79df468ee2a116923fade443957f1
柴志远 <wb.chaizhiyuan01@mesg.corp.netease.com>
2025-06-05 09:24:16 +0800

    feat: init

--- src/components/ImageList/index.tsx
+++ src/components/ImageList/index.tsx
@@ -23,7 +23,7 @@ const ImageList: React.FC<ImageListProps> = ({ images }) => {
           <Image
             width={100}
             height={100}
-            src={image}
+            src={'/' + image}
             alt={`图片 ${index + 1}`}
             className={styles.image}
             preview={{
